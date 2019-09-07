374cf8e1445cb665d648905e1c04f6c4182ae3d7
amata1219 <suzukiamata@gmail.com>
2019-09-08 03:55:50 +0900

    こみっとー

--- src/graffiti/Option.java
+++ src/graffiti/Option.java
@@ -6,9 +6,22 @@ public class Option<T> {
 
 	private static final Option<?> EMPTY = new Option<>(null);
 
+	public static void main(String[] $){
+		Option<String> option1 = unit("A");
+		Option<String> option2 = option1.flatMap(Option::unit);
+
+		System.out.println(option1.toString());
+		System.out.println(option2.toString());
+		System.out.println(option1.toString().equals(option2.toString()));
+	}
+
 	@SuppressWarnings("unchecked")
 	public static <T> Option<T> empty(){
 		return (Option<T>) EMPTY;
+	}
+
+	public static <T> Option<T> unit(T value){
+		return new Option<>(value);
 	}
 
 	private final T value;
@@ -17,20 +30,21 @@ public class Option<T> {
 		this.value = value;
 	}
 
-	public Option<T> unit(T value){
-		return new Option<>(value);
-	}
-
 	public <U> Option<U> map(Function<T, ? extends U> mapper) {
 		return isEmpty() ? new Option<>(mapper.apply(value)) : empty();
 	}
 
 	public <U> Option<U> flatMap(Function<T, Option<U>> mapper){
-		return isEmpty() ? mapper.apply(value) : empty();
+		return isEmpty() ? empty() : mapper.apply(value);
 	}
 
 	public boolean isEmpty(){
 		return value == null;
+	}
+
+	@Override
+	public String toString(){
+		return isEmpty() ? "Option.empty" : "Option." + value.toString();
 	}
 
 }
